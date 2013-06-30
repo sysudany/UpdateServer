f85d095434d34dd297cc808e58e41b597d45bd0e
sysudany <chdany@qq.com>
2013-06-30 13:29:28 +0800

    test passed

--- src/cn/chdany/_3d_images/servlet/UpdateRelWebsitesServlet.java
+++ src/cn/chdany/_3d_images/servlet/UpdateRelWebsitesServlet.java
@@ -1,11 +1,18 @@
 package cn.chdany._3d_images.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+
+import cn.chdany._3d_images.bean.RelWebsite;
 
 @WebServlet("/UpdateRelWebsitesServlet")
 public class UpdateRelWebsitesServlet extends HttpServlet {
@@ -16,7 +23,16 @@ public class UpdateRelWebsitesServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().write("haha");
+		response.setHeader("Content-Type", "text/html;charset=UTF-8");
+		ArrayList<RelWebsite> websites = new ArrayList<RelWebsite>();
+		Random random = new Random();
+		for(int i = 0; i < 100; i++){
+			RelWebsite website = new RelWebsite();
+			website.setId(random.nextLong() + "地址的写法");
+			websites.add(website);
+		}
+		response.getWriter().write("h地址的写法.avia\n");
+		response.getWriter().write(JSON.toJSONString(websites));
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
